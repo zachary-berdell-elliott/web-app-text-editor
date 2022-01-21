461adbfdf35f34172c9cdd1c939547a482e3ec85
Zachary Elliott <zacharyberdell@gmail.com>
2022-01-21 13:29:13 -0700

    Logic added for getting data from the database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,6 +27,18 @@ export const putDb = async (content) => {
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  return result;
+}
 
 initdb();
